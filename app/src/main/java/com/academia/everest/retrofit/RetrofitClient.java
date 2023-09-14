cdbfac0f21f05ae52a8cb379afa40678d5757f56
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2023-09-14 17:25:51 +0545

    refactor retroClient

--- app/src/main/java/com/academia/everest/retrofit/RetrofitClient.java
+++ app/src/main/java/com/academia/everest/retrofit/RetrofitClient.java
@@ -1,6 +1,5 @@
 package com.academia.everest.retrofit;
 
-import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -8,18 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static final String BASE_URL = "http://103.94.159.179:9019/ebl/";
 
-    private static Retrofit retrofit = null;
-
     public static Retrofit getClient() {
-        if (retrofit == null) {
-            OkHttpClient okHttp = new OkHttpClient.Builder().build();
-            return new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(okHttp).build();
-        }
-        return retrofit;
+        OkHttpClient okHttp = new OkHttpClient.Builder().build();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttp).build();
     }
-
-
 }
